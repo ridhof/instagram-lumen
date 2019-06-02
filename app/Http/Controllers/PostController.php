97cc1f559fbb817b06bdd383adf9e887fc84fe2b
Ridho <officialridhof@gmail.com>
2019-06-02 18:10:00 +0700

    fix by adding db on each function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,43 +18,81 @@ class PostController extends Controller
     }
 
     public function index(){
+        $post = DB::table('posting')->get();
         $data = array();
-        $data[] = array(
-            'id_post'=>1,
-            'username'=>'ridho',
-            'tanggal'=>'2 Juni 2019',
-            'caption'=>'Liburan'
-        );
+        foreach($post as $row){
+            $date=date_create($row->tanggal);
+            $date=date_format($date,"d M Y");
+            $data[] = array(
+                'id_post'=>$row->idposting, //number
+                'username'=>$row->username, //string
+                'tanggal'=>$date, //string with format ex: 2 Juni 2019
+                'caption'=>$row->komen // string
+            );
+        }
         return response()->json(array('data'=>$data));
     }
 
     public function thumbnail($username){
+        $thumbnail = DB::table('gambar')
+                            ->select('gambar.idposting', 'gambar.idgambar', 'gambar.extention', 'posting.username')
+                            ->join('posting', 'gambar.idposting', '=', 'posting.idposting')
+                            ->where('posting.username', '=', $username)
+                            ->get();
+        $url = "localhost:8000/public/images/";
         $data = array();
-        $data[] = array(
-            'id_gambar'=>1,
-            'url_gambar'=>'localhost:8000/public/images/1.png'
-        );
+        $idpost_cek = array();
+        foreach($thumbnail as $row){
+            //check does id post of this photos already in array or not
+            //data must be unique posting.id
+            if(!in_array($row->idposting, $idpost_cek)){
+                $idpost_cek[] = $row->idposting;
+                $data[] = array(
+                    'id_gambar'=>$row->idgambar, //number
+                    'url_gambar'=>$url.$row->idgambar.$row->extention //string url to each images
+                );
+            }
+        }
         return response()->json(array('data'=>$data));
     }
 
     public function postById($id){
         $gambar = array();
-        $gambar[] = array(
-            'id_gambar'=>1,
-            'url_gambar'=>'localhost:8000/public/images/1.png'
-        );
+        $gambar_query = DB::table('gambar')
+                            ->where('gambar.idposting', '=', $id)
+                            ->get();
+        $url = "localhost:8000/public/images/";
+        foreach($gambar_query as $row){
+            $gambar[] = array(
+                'id_gambar'=>$row->idgambar, //number
+                'url_gambar'=>$url.$row->idgambar.$row->extention //string url to each images
+            );
+        }
         $komentar = array();
-        $komentar[] = array(
-            'id_komen'=>1,
-            'username'=>'ridho',
-            'isi_komen'=>'keren!'
-        );
+        $komentar_query = DB::table('balasan_komen')
+                                ->where('balasan_komen.idposting', '=', $id)
+                                ->get();
+        foreach($komentar_query as $row){
+            $komentar[] = array(
+                'id_komen'=>$row->idbalasan_komen,
+                'username'=>$row->username,
+                'isi_komen'=>$row->isi_komen
+            );
+        }
+        $post = DB::table('posting')
+                        ->where('posting.idposting', '=', $id)
+                        ->first();
+        $date=date_create($post->tanggal);
+        $date=date_format($date,"d M Y");
+        $jumlah_like = DB::table('jempol_like')
+                        ->where('jempol_like.idposting', '=', $id)
+                        ->count();
         $data = array(
             'id_post'=>$id,
-            'username'=>'ridho',
-            'tanggal'=>'2 Juni 2019',
-            'caption'=>'Liburan!',
-            'jumlah_like'=>5,
+            'username'=>$post->username,
+            'tanggal'=>$date,
+            'caption'=>$post->komen,
+            'jumlah_like'=>$jumlah_like,
             'gambar'=>$gambar,
             'komentar'=>$komentar
         );
@@ -62,32 +100,65 @@ class PostController extends Controller
     }
 
     public function createComment(Request $request){
+        DB::table('balasan_komen')->insert(
+            [
+                'idposting'=>$request->id_post, 
+                'username'=>$request->commenting_username, 
+                'isi_komen'=>$request->komentar,
+                'tanggal'=>date('Y-m-d G:i:s')
+            ]
+        );
+        $latest = DB::table('balasan_komen')
+                        ->orderBy('idbalasan_komen', 'desc')
+                        ->first();
         $data = array(
             'status'=>1,
-            'id_komen'=>1,
+            'id_komen'=>$latest->idbalasan_komen,
             'commenting_username'=>$request->commenting_username,
             'id_post'=>$request->id_post,
             'komentar'=>$request->komentar,
-            'tanggal'=>'2 Juni 2019'
+            'tanggal'=>date('Y-m-d G:i:s')
         );
         return response()->json(array('data'=>$data));
     }
 
     public function likeCertainUser($id, $username){
+        $like_status = DB::table('jempol_like')
+                        ->select('jempol_like.idposting', 'posting.username')
+                        ->where('jempol_like.idposting', '=', $id)
+                        ->where('jempol_like.username', '=', $username)
+                        ->count();
         $data = array(
             'id_post'=>$id,
             'liking_username'=>$username,
-            'like'=>1
+            'like'=>$like_status
         );
         return response()->json(array('data'=>$data));
     }
 
     public function createLike(Request $request){
+        $like = 1;
+        if($request->like == 1){
+            DB::table('jempol_like')->insert(
+                [
+                    'idposting'=>$request->id_post, 
+                    'username'=>$request->liking_username
+                ]
+            );
+        }
+        else{
+            DB::table('jempol_like')
+                    ->where('idposting', '=', $request->id_post)
+                    ->where('username', '=', $request->liking_username)
+                    ->delete();
+            $like = 0;
+        }
+        
         $data = array(
             'status'=>1,
             'id_post'=>$request->id_post,
             'liking_username'=>$request->liking_username,
-            'like'=>0
+            'like'=>$like
         );
         return response()->json(array('data'=>$data));
     }
